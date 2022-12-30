8c476298909f6ca19dd1b0e293cc8f40dd8a36d3
Farah^_^,Abderrazzak <108276410+FrhXM@users.noreply.github.com>
2022-12-30 21:34:36 +0100

    Add files via upload

---
+++ pkg.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/sh
+sudo pacman -S --needed xorg-server xorg-xinit xorg-apps xwallpaper qtile kitty rofi \
+	firefox qutebrowser \
+	neovim sxiv mpv \
+	nemo udisks2 udiskie dosfstools ntfs-3g \
+	gvfs gvfs-mtp gvfs-smb gvfs-afc gvfs-nfs gvfs-gphoto2 \
+	scrot slock dunst libnotify \
+	fzf fd exa bat xclip trash-cli man-db imagemagick \
+	arc-gtk-theme arc-icon-theme lxappearance-gtk3 unclutter picom \
+	pipewire pipewire-pulse wireplumber pipewire-alsa pipewire-jack pipewire-zeroconf alsa-utils pulsemixer \
+	bluez bluez-utils blueman network-manager-applet \
+	xf86-video-intel xdg-user-dirs pacman-contrib neofetch 
