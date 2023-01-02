82a38729459118d81ded379e75cb05bda511037b
Farah^_^,Abderrazzak <108276410+FrhXM@users.noreply.github.com>
2023-01-02 17:14:15 +0100

    Add files via upload

--- pkg.sh
+++ pkg.sh
@@ -4,9 +4,9 @@ sudo pacman -S --needed xorg-server xorg-xinit xorg-apps xwallpaper qtile kitty 
 	neovim sxiv mpv \
 	nemo udisks2 udiskie dosfstools ntfs-3g \
 	gvfs gvfs-mtp gvfs-smb gvfs-afc gvfs-nfs gvfs-gphoto2 \
-	scrot slock dunst libnotify \
+	slock dunst libnotify \
 	fzf fd exa bat xclip trash-cli man-db imagemagick \
-	arc-gtk-theme arc-icon-theme lxappearance-gtk3 unclutter picom \
+	arc-gtk-theme papirus-icon-theme lxappearance-gtk3 unclutter picom \
 	pipewire pipewire-pulse wireplumber pipewire-alsa pipewire-jack pipewire-zeroconf alsa-utils pulsemixer \
 	bluez bluez-utils blueman network-manager-applet \
-	xf86-video-intel xdg-user-dirs pacman-contrib neofetch 
+	xf86-video-intel xdg-user-dirs pacman-contrib neofetch
