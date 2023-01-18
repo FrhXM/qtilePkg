2356fd05db5fddee210e8814fc53c4b9e50e5f5c
Farah^_^,Abderrazzak <108276410+FrhXM@users.noreply.github.com>
2023-01-18 14:26:01 +0100

    Add files via upload

--- pkg.sh
+++ pkg.sh
@@ -5,7 +5,7 @@ sudo pacman -S --needed xorg-server xorg-xinit xorg-apps xwallpaper qtile kitty 
 	nemo udisks2 udiskie dosfstools ntfs-3g \
 	gvfs gvfs-mtp gvfs-smb gvfs-afc gvfs-nfs gvfs-gphoto2 \
 	slock dunst libnotify \
-	fzf fd exa bat xclip trash-cli man-db imagemagick \
+	fzf fd exa bat xclip trash-cli man-db imagemagick flameshot \
 	arc-gtk-theme papirus-icon-theme lxappearance-gtk3 unclutter picom \
 	pipewire pipewire-pulse wireplumber pipewire-alsa pipewire-jack pipewire-zeroconf alsa-utils pulsemixer \
 	bluez bluez-utils blueman network-manager-applet \
